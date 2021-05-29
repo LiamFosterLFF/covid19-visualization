7381301c73d7abdafdfcf05b38974ae5ff944e1e
lf639 <lf639@bath.ac.uk>
2021-05-29 15:04:16 +0100

    Map: Cleaned up functions and removed dead code, also changed the way map is changed so it uses stateful methods

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -6,11 +6,11 @@ import { VectorMap } from '@south-paw/react-vector-maps';
 import countryNameDictionary from '../../countryNameDictionary.js';
 import Rainbow from 'rainbowvis.js';
 
-
-
 const Map = (props) => {
 
     const [ mapStats, setMapStats ] = useState({})
+    const [ provinceColors, setProvinceColors ] = useState("");
+    const [ map, setMap ] = useState(componentJsonDictionary["worldLowRes"]);
 
     const calculateMapStats = (countryData) => {
         const provinceStats = {};
@@ -31,44 +31,6 @@ const Map = (props) => {
         setMapStats(calculateMapStats(props.data))
     }, [props.data])
 
-    // const sortDataByDate = (data) => {
-    //     if (data) {
-    //         // Takes data (time series object for chosen country) and turns into sorted array of form [[date, value],]
-    //         return (Object.entries(data).sort((a, b) => {
-    //             // Split date strings into segments, then sort by Y, M, D (US format dates)
-    //             const [aArr, bArr] = [a[0].split('/'), b[0].split('/')]
-    //             const checkingOrder = [2, 0, 1];
-    //             for (let i = 0; i < checkingOrder.length; i++) {
-    //                 const c = checkingOrder[i];
-    //                 const [aVal, bVal] = [Number.parseInt(aArr[c]), Number.parseInt(bArr[c])]
-    //                 if (aVal > bVal) {
-    //                     return 1;
-    //                 } else if (bVal > aVal) {
-    //                     return -1;
-    //                 }
-    //             }
-    //             return 0
-    //         }))
-    //     }
-    // }
-    // console.log(mapData);
-
-    const onClick = ({ target }) => {
-        const id = target.attributes.id.value;
-        if (id in componentJsonDictionary) {
-            props.setCountry(countryNameDictionary[id])
-            setComponent(<VectorMap {...componentJsonDictionary[id]}/>)
-        } else {
-            
-        }
-    }
-
-    const [component, setComponent] = useState(<VectorMap {...componentJsonDictionary["worldLowRes"]} layerProps={{ onClick }} />);
-    
-
-    
-    const [ provinceColors, setProvinceColors ] = useState("");
-
     useEffect(() => {
         if (Object.keys(mapStats).length !== 0) {
             let newProvinceColors = "";
@@ -78,7 +40,6 @@ const Map = (props) => {
             for (let [key, value] of Object.entries(countryNameDictionary)) {
                 countryIdDictionary[value] = key;
             }
-            console.log(countryIdDictionary);
 
             // Set a custom color gradient from 0=green to 100=red
             const rainbow = new Rainbow();
@@ -86,50 +47,14 @@ const Map = (props) => {
 
             Object.entries(mapStats).forEach(([provinceName, provinceStat]) => {
                 const provinceId = countryIdDictionary[provinceName.toLowerCase()];
-                console.log(provinceName, provinceId);
                 if (provinceId !== undefined) { // Object is in dictionary (not a boat or small country etc)
                     const hue = rainbow.colourAt(Math.floor(provinceStat))
                     newProvinceColors += `&[id="${provinceId}"] {fill: #${hue}}`
                 }
             })
-            console.log(newProvinceColors);
             setProvinceColors(newProvinceColors)
         }
     }, [mapStats])
-    // if ((props.country === "world") && (Object.keys(props.data).length !== 0)) { // Before data has loaded, object will be empty
-    //     // console.log("a", props.country, props.data["world"][props.dataType].statistics, props.data);
-        
-    //     for (let [key, value] of Object.entries(props.data["world"][props.dataType].statistics)) { // Get statistics of recent deaths
-    //         const id = countryIdDictionary[key];
-    //         if (id !== undefined) { // Object is in dictionary (not a boat or small country etc)
-                
-    //             // Fill country with color going from green (low proportionate rise in cases) to red (high rise)
-    //             // Percentage as a percentage of 30%, representing a doubling time of 2.5 days 
-    //             const rise = (value.total > 0) ? ((value.increase/value.total) / .15) : 0; 
-
-    //             // Set a custom color gradient from 0=green to 100=red
-    //             const rainbow = new Rainbow();
-    //             rainbow.setSpectrum('#2eff00', '#ff0000')
-                
-    //             let hueNo;
-    //             if (props.dataType === "recovered") {
-    //                 hueNo = 100 - rise*100;
-    //             } else if (rise * 100 > 100) {
-    //                 hueNo = 100;
-    //             } else {
-    //                 hueNo = rise * 100;
-    //             }
-                
-    //             const hue = rainbow.colourAt(Math.floor(hueNo))
-                
-    //             provinceFills += `&[id="${id}"] {fill: #${hue}}`
-
-    //         }
-    //     }
-
-    // }
-
-
     
     const MapStyling = styled.div`
     path {
@@ -148,21 +73,27 @@ const Map = (props) => {
 
         
     }`;
-    
 
+    const onClick = ({ target }) => {
+        const id = target.attributes.id.value;
+        if (id in componentJsonDictionary) {
+            props.setCountry(countryNameDictionary[id])
+            setMap(componentJsonDictionary[id])
+        } else {
+            
+        }
+    }
     
     const backClick = () => {
         props.setCountry("world")
-        setComponent(<VectorMap {...componentJsonDictionary["worldLowRes"]} layerProps={{ onClick }} />)
+        setMap(componentJsonDictionary["worldLowRes"]y)
     }
-
-
 
     return (
         <div>
             <MapStyling>
                 <button onClick={backClick}>World Map</button>
-                {component}
+                <VectorMap {...map} layerProps={{ onClick }} />
             </MapStyling>
 
         </div>
