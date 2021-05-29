6bffd19bd2b5582eb0fc812a6ae0cb05082a0a0f
lf639 <lf639@bath.ac.uk>
2021-05-29 15:41:25 +0100

    Charts: Total chart now displays date as it is once again getting this data in its input data

--- src/components/Charts/TotalChart.js
+++ src/components/Charts/TotalChart.js
@@ -11,6 +11,7 @@ const TotalChart = ({ data }) => {
       const ld = []
       for (let i = 0; i < data["confirmed"].length; i++) {
         ld.push({
+          "date": data["confirmed"][i].date,
           "confirmed": data["confirmed"][i].total,
           "recovered": data["recovered"][i].total,
           "deaths": data["deaths"][i].total,
